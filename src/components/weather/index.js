7fc1fd82d7b209fa218f7ce9d53e8adb556ba46b
sthabkash <sthabkash@gmail.com>
2022-05-16 18:22:56 -0400

    antd install and remove default design

---
+++ src/components/weather/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Row} from 'antd'
+const Weather = () => {
+    return (<Row>
+        <div>Hello weather app</div>;
+        </Row>
+    );
+};
+export default Weather;
